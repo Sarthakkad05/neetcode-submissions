5ae3a9bde4aaa9c7f69b35a26ffef2a14f095ee1
Sarthak Kad <kadsarthak33@gmail.com>
2026-06-06 15:19:37 +0530

    Add: roman-to-integer - submission-3

---
+++ Data Structures & Algorithms/roman-to-integer/submission-3.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character,Integer> mp = new HashMap<>();
+        mp.put('I',1);
+        mp.put('V',5);
+        mp.put('X',10);
+        mp.put('L',50);
+        mp.put('C',100);
+        mp.put('D',500);
+        mp.put('M',1000);
+
+        int result = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            int current = mp.get(s.charAt(i));
+
+            if(i < s.length() - 1 && current < mp.get(s.charAt(i+1))){
+                result -= current;
+            }else{
+                result += current;
+            }
+        }
+        return result;
+    }
+}
