5ad8cca473c2224f0f704465f7c98d8883e1a230
Sarthak Kad <kadsarthak33@gmail.com>
2026-06-30 16:04:33 +0530

    Add: design-hashmap - submission-0

---
+++ Data Structures & Algorithms/design-hashmap/submission-0.java
@@ -0,0 +1,41 @@
+class MyHashMap {
+
+    private List<int[]> map;
+
+    public MyHashMap() {
+        map = new ArrayList<>();
+    }
+
+    public void put(int key, int value) {
+
+        for (int[] pair : map) {
+            if (pair[0] == key) {
+                pair[1] = value;
+                return;
+            }
+        }
+
+        map.add(new int[]{key, value});
+    }
+
+    public int get(int key) {
+
+        for (int[] pair : map) {
+            if (pair[0] == key) {
+                return pair[1];
+            }
+        }
+
+        return -1;
+    }
+
+    public void remove(int key) {
+
+        for (int i = 0; i < map.size(); i++) {
+            if (map.get(i)[0] == key) {
+                map.remove(i);
+                return;
+            }
+        }
+    }
+}
