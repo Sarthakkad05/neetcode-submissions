56f8289d6e4ed0fa94457c188810bbcf67f8fd25
Sarthak Kad <kadsarthak33@gmail.com>
2026-08-21 16:31:36 +0530

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    List<List<Integer>> ans = new ArrayList<>();
+
+    public List<List<Integer>> permute(int[] nums) {
+        backtrack(0, nums);
+        return ans;
+    }
+
+    private void backtrack(int index, int[] nums){
+
+        if(index == nums.length){
+
+            List<Integer> perm = new ArrayList<>();
+
+            for(int num: nums){
+                perm.add(num);
+            }
+
+            ans.add(perm);
+        }
+
+        for(int i = index; i < nums.length; i++){
+            
+            swap(nums, index,i);
+            backtrack(index + 1, nums);
+            swap(nums, index, i);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j){
+
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
